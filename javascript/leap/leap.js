9da137ba3aeee84a24f707a0943779c42f579eef
panox <panospalos@gmail.com>
2015-12-20 18:47:00 +0000

    every 400 years leap year true

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,7 +3,10 @@ var Year = function(year) {
 };
 
 Year.prototype.isLeap = function() {
-  if (this.year % 100 === 0) {
+  if (this.year % 400 === 0 ) {
+    return true;
+  }
+  else if (this.year % 100 === 0) {
     return false;
   }
   else if (this.year % 4 === 0) {
