8397e9b1253d618d3ac0f674992e54bcf7ee8528
panox <panospalos@gmail.com>
2015-12-21 23:36:02 +0200

    filter the two arrays for difference and return the lenght

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,17 +1,8 @@
 var Hamming = function() {};
 
 Hamming.prototype.compute = function(a, b) {
-  var aSplit = a.split('');
-  var bSplit = b.split('');
-
-  var ret = [];
-  for(var i in aSplit) {
-    if(bSplit.indexOf( aSplit[i] ) > -1){
-      ret.push( aSplit[i] );
-    }
-  }
-  return ret.length;
-
+  var aSplit = a.split(''), bSplit = b.split('');
+  return aSplit.filter(function(i) {return bSplit.indexOf(i) < 0;}).length;
 };
 
 module.exports = Hamming;
