29abedc3a8261d81d4a4f174d967168213f3ffb7
panox <panospalos@gmail.com>
2015-12-22 00:05:52 +0200

    6 tests pass

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,18 +1,23 @@
 var Hamming = function() {};
 
-Hamming.prototype.compute = function(a, b) {
-  var aSplit = a.split(''), bSplit = b.split('');
-  var equals = [];
+Hamming.prototype.compute = function(a, b) {  
+  if (a == b ) {
+    return 0;
+  }
+  else {
+    var aSplit = a.split(''), bSplit = b.split('');
+    var equals = [];
 
-  aSplit.forEach( function (letter, index) {
-    if (letter == bSplit[index]) {
-      equals.push(true);
-    }
-    else  {
-      equals.push(false);
-    }
-  });
-  return equals.toString().match(/false/g).length;
+    aSplit.forEach( function (letter, index) {
+      if (letter == bSplit[index]) {
+        equals.push(true);
+      }
+      else  {
+        equals.push(false);
+      }
+    });
+    return equals.toString().match(/false/g).length;
+  }
 };
 
 module.exports = Hamming;
