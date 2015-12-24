a82d24e4b2106c7728e11c82f20c327d80f71e77
panox <panospalos@gmail.com>
2015-12-24 17:41:30 +0200

    all tests pass with map and regex replace

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,11 +1,17 @@
 var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(nucleotide) {
-    var Rna = nucleotide
-    .replace(/C/g, 'G')
-    .replace(/G/g, 'C')
-    .replace(/A/g, 'U')
-    .replace(/T/g, 'A');
+
+    var map = {
+        C:"G",
+        G:"C",
+        A:"U",
+        T:"A"
+    };
+
+    Rna = nucleotide.replace(/G|C|T|A/g, function(matched){
+        return map[matched];
+    });
     return Rna;
 };
 
