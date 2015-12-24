476784d19024085bc0e9ccd74bf65f917f6fcee5
panox <panospalos@gmail.com>
2015-12-24 17:16:39 +0200

    module for rna created

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,9 @@
+var DnaTranscriber = function() {};
+
+DnaTranscriber.prototype.toRna = function(nucleotide) {
+    //
+    // Your solution to the exercise goes here
+    //
+};
+
+module.exports = DnaTranscriber;
