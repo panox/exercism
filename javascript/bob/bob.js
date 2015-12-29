ae9c376bd9f598b454db91f53635ea29661b66d7
panox <panospalos@gmail.com>
2015-12-29 14:47:18 +0200

    test one pass

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,7 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  return 'Whatever.';
 };
 
 module.exports = Bob;
