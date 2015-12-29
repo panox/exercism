f8fb20e2b65a50248a80d892ebb9832a47c53a25
panox <panospalos@gmail.com>
2015-12-29 14:50:35 +0200

    test two passes

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,12 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  return 'Whatever.';
+  if (input === input.toUpperCase()) {
+    return 'Whoa, chill out!';
+  }
+  else {
+    return 'Whatever.';
+  }
 };
 
 module.exports = Bob;
