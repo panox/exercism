1cc9872b38d7a5b6faa55d7e2ce2c4408a6446db
panox <panospalos@gmail.com>
2016-01-01 14:20:51 +0200

    all tests pass

--- javascript/gigasecond/gigasecond.spec.js
+++ javascript/gigasecond/gigasecond.spec.js
@@ -20,7 +20,7 @@ describe('Gigasecond', function() {
     expect(gs.date()).toEqual(expectedDate);
   });
 
-  xit('make sure calling "date" doesn\'t mutate value', function() {
+  it('make sure calling "date" doesn\'t mutate value', function() {
     var gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
     var expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
     gs.date();
