b75ddb93f51137fdf1cd95b940fe98fbbff4f814
panox <panospalos@gmail.com>
2016-01-01 14:20:30 +0200

    third test passes

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -6,8 +6,8 @@ Gigasecond.prototype.date = function() {
   var startDate = this.startDate;
   var gigasecond = Math.pow(10,9);
   if (startDate.getTime() < 0 ) {
-    endDate = startDate.getTime() / 1000 + gigasecond;
-    return endDate;
+    endDateSeconds = startDate.getTime() + gigasecond*1000;
+    return new Date(endDateSeconds);
   }
   startDate.setSeconds(startDate.getSeconds() + gigasecond);
   return startDate;
