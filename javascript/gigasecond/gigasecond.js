87065f7300f56643af016b2deec404b47062d9b7
panox <panospalos@gmail.com>
2016-01-01 13:40:32 +0200

    starting date set in construction function

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,7 +1,9 @@
-var Gigasecond = function() {};
+var Gigasecond = function(startDate) {
+  this.startDate = startDate;
+};
 
-Gigasecond.prototype.date = function(what) {
-
+Gigasecond.prototype.date = function() {
+  return this.startDate;
 };
 
 module.exports = Gigasecond;
