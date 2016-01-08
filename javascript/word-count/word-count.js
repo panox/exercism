509c423e07270898b3533d68a1bbec14725c8296
panox <panospalos@gmail.com>
2016-01-08 17:44:31 +0000

    return one word Object

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,6 +3,8 @@ var Words = function() {};
 Words.prototype.count = function(phrase) {
   var arrayOfWords = phrase.split(" ");
   var objOfWords = {};
+  objOfWords[arrayOfWords[0]] = 1;
+  return objOfWords;
 };
 
 module.exports = Words;
