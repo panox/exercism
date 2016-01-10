0442fad460d520bd2d2a1e06882c94e8fe018c1e
panox <panospalos@gmail.com>
2016-01-10 20:53:09 +0000

    initial statement on song before ifs

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -16,16 +16,12 @@ BeerSong.prototype.verse = function(number) {
 };
 
 BeerSong.prototype.sing = function(startNumber, endNumber) {
-  var song = "";
+  var song = startNumber+ ' bottles of beer on the wall, '+startNumber+' bottles of beer.\n';
   if (startNumber === 1) {
     song += '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
   }
   else if (startNumber === 0 ) {
     song += 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
-  }
-  else {
-    song = startNumber+ ' bottles of beer on the wall, '+startNumber+' bottles of beer.\n';
-    song += 'Take one down and pass it around, '+(startNumber-1)+' bottles of beer on the wall.\n' ;
   }
   return song;
 };
