34d0514d970eb164bd058caf5345f61578080e4b
panox <panospalos@gmail.com>
2016-01-10 17:01:13 +0000

    regex to match the 26 characters of the alphabet

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -6,6 +6,10 @@ Pangram.prototype.isPangram = function() {
   if (this.phrase === '') {
     return false;
   }
+  else  {
+    var regex = /([a-z])(?!.*\1)/g;
+    return (this.phrase.match(regex)).length === 26;
+  }
 };
 
 module.exports = Pangram;
