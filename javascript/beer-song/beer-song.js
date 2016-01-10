793e10023d07b1d03b1b67255b6c471a2ac9b2a2
panox <panospalos@gmail.com>
2016-01-10 17:37:08 +0000

    function takes start and end numbers

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,16 +1,16 @@
 var BeerSong = function() {};
 
-BeerSong.prototype.verse = function(number) {
+BeerSong.prototype.verse = function(startNumber, endNumber) {
   var song = "";
-  if (number === 1) {
+  if (startNumber === 1) {
     song += '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
   }
-  else if (number === 0 ) {
+  else if (startNumber === 0 ) {
     song += 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
   }
   else {
-    song = number+ ' bottles of beer on the wall, '+number+' bottles of beer.\n';
-    song += 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
+    song = startNumber+ ' bottles of beer on the wall, '+startNumber+' bottles of beer.\n';
+    song += 'Take one down and pass it around, '+(startNumber-1)+' bottles of beer on the wall.\n' ;
   }
   return song;
 };
