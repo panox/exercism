ff725fc3a6f9c95db1ecccaeaef5d9e5c84a03ce
panox <panospalos@gmail.com>
2016-01-10 17:24:44 +0000

    first test passes with -1 hardcoded

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -2,8 +2,8 @@ var BeerSong = function() {};
 
 BeerSong.prototype.verse = function(number) {
   var song;
-  song += number+ ' bottles of beer on the wall, '+number+' bottles of beer.\n';
-  song += 'Take one down and pass it around, '+number-1+' bottles of beer on the wall.\n' ;
+  song = number+ ' bottles of beer on the wall, '+number+' bottles of beer.\n';
+  song += 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
   return song;
 };
 
