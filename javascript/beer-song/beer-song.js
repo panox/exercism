ee4ff6ecf61b5fe7566eb1f746aea1af576eda16
panox <panospalos@gmail.com>
2016-01-10 17:28:43 +0000

    if statement for 0 number

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -6,6 +6,9 @@ BeerSong.prototype.verse = function(number) {
   if (number === 1) {
     song = '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
   }
+  else if (number === 0 ) {
+    song = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+  }
   else {
     song += 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
   }
