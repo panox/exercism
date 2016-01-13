6b58d3b86c312f9ec4bd19193f53ba87bc4e4e61
panox <panospalos@gmail.com>
2016-01-13 15:32:21 +0000

    result looks like expected

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -19,13 +19,16 @@ BeerSong.prototype.sing = function(startNumber, endNumber) {
   endNumber = endNumber || 1;
   var song = "";
   for (var i = startNumber; i >= endNumber; i--) {
-    song += i.toString() + ' bottles of beer on the wall, '+ i.toString() +' bottles of beer.\n';
+
 
     if (i === 1) {
-      song += 'Take it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+      song += '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
     }
     else {
-      song += 'Take one down and pass it around, ' + (i-1).toString() + ' bottles of beer on the wall.\n';
+      song += i.toString() + ' bottles of beer on the wall, '+ i.toString() +' bottles of beer.\n';
+      song += 'Take one down and pass it around, ' + (i-1).toString() + ' bottle';
+      if (i !== 1) { song += 's';}
+      song += ' of beer on the wall.\n';
     }
 
     if (i !== endNumber) {
