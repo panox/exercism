5817ec08117f1638820c04232c4ea80b1aef188b
panox <panospalos@gmail.com>
2016-01-13 15:39:11 +0000

    number object property

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,6 +1,9 @@
-var PhoneNumber = function() {};
+var PhoneNumber = function(number) {
+  this.number = number;
+};
 
 PhoneNumber.prototype.number = function() {
+  return this.number;
 };
 
 module.exports = PhoneNumber;
