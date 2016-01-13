6cdfde462add909df38e12f75005f3e70104b929
panox <panospalos@gmail.com>
2016-01-13 15:56:10 +0000

    if statement to check if 11 characters and first is 1

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -5,6 +5,9 @@ var PhoneNumber = function(phone) {
 PhoneNumber.prototype.number = function() {
   var re = /[^\d]/g;
   var result = this.phone.replace(re, "");
+  if (result.length === 11 && result[0] === "1") {
+    return "hi";
+  }
   return result;
 };
 
