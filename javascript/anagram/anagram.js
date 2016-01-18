f65345c4854b7bd23b4e3492041eb43673c068f5
panox <panospalos@gmail.com>
2016-01-18 16:53:19 +0000

    array argument word variable

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,8 +2,8 @@ var Anagram = function(word) {
     this.word = word;
 };
 
-Anagram.prototype.matches = function() {
-    return this.word;
+Anagram.prototype.matches = function(array) {
+    var word = this.word;
 };
 
 module.exports = Anagram;
