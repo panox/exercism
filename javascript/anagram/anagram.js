cff52033f24498d0c8d427bfd11c72ede973d4b8
panox <panospalos@gmail.com>
2016-01-18 17:46:43 +0000

    for each run compare many times

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -5,19 +5,19 @@ var Anagram = function(word) {
 Anagram.compare = function(one, two) {
     one = one.split("").sort().join();
     two = two.split("").sort().join();
-    if (one.lenght !== two.length) {
-        return [];
-    }
     return one === two;
 };
 
 Anagram.prototype.matches = function(array) {
     var word = this.word;
-    var result;
     array.forEach(function(entry) {
-        result = Anagram.compare(word, entry);
+        if (Anagram.compare(word, entry)) {
+            return entry;
+        }
+        else {
+            return [];
+        }
     });
-    return result;
 };
 
 module.exports = Anagram;
