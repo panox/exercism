f207519f188970c4c12ce9726b4cba56cd747b04
panox <panospalos@gmail.com>
2016-01-19 15:41:41 +0000

    verse function return after if statement

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,39 +1,22 @@
 var BeerSong = function() {};
 
 BeerSong.prototype.verse = function(number) {
-  var song = "";
   if (number === 1) {
-    song += '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
+    return '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
   }
   else if (number === 0 ) {
-    song += 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+    return 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
   }
   else {
-    song = number+ ' bottles of beer on the wall, '+number+' bottles of beer.\n';
-    song += 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
+    return +number+' bottles of beer on the wall, '+number+' bottles of beer.\n' + 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
   }
-  return song;
 };
 
 BeerSong.prototype.sing = function(startNumber, endNumber) {
   endNumber = endNumber || 1;
   var song = "";
   for (var i = startNumber; i >= endNumber; i--) {
-
-
-    if (i === 1) {
-      song += '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
-    }
-    else {
-      song += i.toString() + ' bottles of beer on the wall, '+ i.toString() +' bottles of beer.\n';
-      song += 'Take one down and pass it around, ' + (i-1).toString() + ' bottle';
-      if (i !== 1) { song += 's';}
-      song += ' of beer on the wall.\n';
-    }
-
-    if (i !== endNumber) {
-      song += '\n';
-    }
+    song += this.verse(1);
   }
   return song;
 };
