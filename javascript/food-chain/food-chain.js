868c4f1f788758264c8101aff68bc0468f135808
panox <panospalos@gmail.com>
2016-01-21 14:11:49 +0000

    map array with the two animals

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -2,6 +2,7 @@ var FoodChain = function() {};
 
 FoodChain.prototype.verse = function(number) {
   var song = "";
+  var map = ['fly', 'spider'];
   song += 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly.';
   song += ' Perhaps she\'ll die.\n';
   return song;
