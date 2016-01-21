091798924162e46c22936efaadb55c79ed81fb25
panox <panospalos@gmail.com>
2016-01-21 15:21:23 +0000

    last line does not need line breaks

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -8,7 +8,7 @@ BeerSong.prototype.verse = function(number) {
     return 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
   }
   else {
-    return +number+' bottles of beer on the wall, '+number+' bottles of beer.\n' + 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
+    return +number+' bottles of beer on the wall, '+number+' bottles of beer.\n' + 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n\n' ;
   }
 };
 
