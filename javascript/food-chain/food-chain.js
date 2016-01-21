9f84e0a41f2dc2ad42d17bca562eff6af99adbbe
panox <panospalos@gmail.com>
2016-01-21 14:41:31 +0000

    adding the fly on first verse dynamically

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -2,9 +2,9 @@ var FoodChain = function() {};
 
 FoodChain.prototype.verse = function(number) {
   var song = "";
-  var map = ['fly', 'spider'];
+  var map = ['empty', 'fly', 'spider'];
   if (number === 1) {
-    song += 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
+    song += 'I know an old lady who swallowed a '+map[number]+'.\nI don\'t know why she swallowed the '+map[number]+'. Perhaps she\'ll die.\n';
   }
   else {
     song += 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n';
