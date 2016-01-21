cac09bef77b4e072f7e5f295b2896fb1473cbb70
panox <panospalos@gmail.com>
2016-01-21 14:51:07 +0000

    begin and end of song stay the same middle changes

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -3,13 +3,15 @@ var FoodChain = function() {};
 FoodChain.prototype.verse = function(number) {
   var song = "";
   var map = ['empty', 'fly', 'spider'];
+  song += 'I know an old lady who swallowed a '+map[number]+'.\n';
   if (number === 1) {
-    song += 'I know an old lady who swallowed a '+map[number]+'.\nI don\'t know why she swallowed the '+map[number]+'. Perhaps she\'ll die.\n';
+    song += 'I don\'t know why she swallowed the '+map[number]+'. '
   }
-  else {
-    song += 'I know an old lady who swallowed a '+map[number]+'.\nIt wriggled and jiggled and tickled inside her.\n';
-    song += 'She swallowed the '+map[number]+' to catch the '+map[number-1]+'.\n' + 'I don\'t know why she swallowed the '+map[number-1]+'. Perhaps she\'ll die.\n';
+  else if (number === 2){
+    song += 'It wriggled and jiggled and tickled inside her.\n';
+    song += 'She swallowed the '+map[number]+' to catch the '+map[number-1]+'.\n' + 'I don\'t know why she swallowed the '+map[number-1]+'. ';
   }
+  song += 'Perhaps she\'ll die.\n';
   return song;
 
 };
