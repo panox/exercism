3507c652b699bd27a39a34c5b6c5d8c4f83622d1
panox <panospalos@gmail.com>
2016-01-21 14:56:34 +0000

    fall through switch statement

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -2,14 +2,13 @@ var FoodChain = function() {};
 
 FoodChain.prototype.verse = function(number) {
   var song = "";
-  var map = ['empty', 'fly', 'spider'];
   song += 'I know an old lady who swallowed a '+map[number]+'.\n';
-  if (number === 1) {
-    song += 'I don\'t know why she swallowed the '+map[number]+'. '
-  }
-  else if (number === 2){
-    song += 'It wriggled and jiggled and tickled inside her.\n';
-    song += 'She swallowed the '+map[number]+' to catch the '+map[number-1]+'.\n' + 'I don\'t know why she swallowed the '+map[number-1]+'. ';
+  switch(number) {
+      case 2:
+        song += 'It wriggled and jiggled and tickled inside her.\n';
+        song += 'She swallowed the spider to catch the fly.\n'
+      case 1:
+        song += 'I don\'t know why she swallowed the fly. ';
   }
   song += 'Perhaps she\'ll die.\n';
   return song;
