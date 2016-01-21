3381db50933631859934665fdb3ac12ae954dc84
panox <panospalos@gmail.com>
2016-01-21 14:08:16 +0000

    fly test passes

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,6 +1,9 @@
 var FoodChain = function() {};
 
-FoodChain.prototype.verse = function() {
+FoodChain.prototype.verse = function(number) {
+
+  return 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
+
 };
 
 module.exports = FoodChain;
