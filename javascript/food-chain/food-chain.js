17777bad1ed93b93e345a1970de6f0233a9182cd
panox <panospalos@gmail.com>
2016-01-21 15:01:39 +0000

    case 2 needs differentiation

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -2,9 +2,12 @@ var FoodChain = function() {};
 
 FoodChain.prototype.verse = function(number) {
   var song = "";
-  var map = ['empty', 'fly', 'spider'];
+  var map = ['empty', 'fly', 'spider', 'bird'];
   song += 'I know an old lady who swallowed a '+map[number]+'.\n';
   switch(number) {
+      case 3:
+      song += 'How absurd to swallow a bird!\n';
+      song += 'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
       case 2:
         song += 'It wriggled and jiggled and tickled inside her.\n';
         song += 'She swallowed the spider to catch the fly.\n'
