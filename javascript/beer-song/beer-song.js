7d32c141924002b17dd459dd82844b29a94ad831
panox <panospalos@gmail.com>
2016-01-21 15:26:36 +0000

    if statement number 2

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -7,6 +7,9 @@ BeerSong.prototype.verse = function(number) {
   else if (number === 0 ) {
     return 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
   }
+  else if (number === 2 ) {
+    return '2 bottles of beer on the wall, 2 bottles of beer.\n' + 'Take one down and pass it around, 1 bottle of beer on the wall.\n' ;
+  }
   else {
     return +number+' bottles of beer on the wall, '+number+' bottles of beer.\n' + 'Take one down and pass it around, '+(number-1)+' bottles of beer on the wall.\n' ;
   }
