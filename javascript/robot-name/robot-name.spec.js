5d96e97d353eadff31c5fc4a6444e64cb0f83cad
panox <panospalos@gmail.com>
2016-02-02 20:49:04 +0200

    about 9000 are unique names

--- javascript/robot-name/robot-name.spec.js
+++ javascript/robot-name/robot-name.spec.js
@@ -47,7 +47,7 @@ describe('Robot', function() {
     expect(originalName).not.toEqual(newName);
   });
 
-  xit('should set a unique name after reset', function() {
+  it('should set a unique name after reset', function() {
     var i,
         numResets = 10000,
         usedNames = {};
