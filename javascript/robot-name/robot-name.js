9ec795cee7d17af37e6cdeab57ad292c9c767543
panox <panospalos@gmail.com>
2016-02-02 20:38:16 +0200

    alphabet and numbers

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -3,6 +3,8 @@ var Robot = function() {
 };
 
 function generatName() {
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' ];
+  var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   return 'RX837';
 }
 
