50755f003fca17dfaa2af064496f1e4371d5bcb7
panox <panospalos@gmail.com>
2016-02-02 20:44:41 +0200

    generate random name from arrays

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -5,7 +5,12 @@ var Robot = function() {
 function generatName() {
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' ];
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  return 'RX837';
+  var char1 = alphabet[Math.floor(Math.random() * 24)];
+  var char2 = alphabet[Math.floor(Math.random() * 24)];
+  var number1 = numbers[Math.floor(Math.random() * 8)];
+  var number2 = numbers[Math.floor(Math.random() * 8)];
+  var number3 = numbers[Math.floor(Math.random() * 8)];
+  return char1+char2+number1+number2+number3;
 }
 
 module.exports = Robot;
