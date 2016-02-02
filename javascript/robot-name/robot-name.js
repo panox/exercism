cab07db919ed08ec7d3cb86343b99882f77dfce1
panox <panospalos@gmail.com>
2016-02-02 21:11:15 +0200

    increase max index

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -2,21 +2,21 @@ var Robot = function() {
   this.name = generatName();
 };
 
-function randomEight() {
-  return Math.floor(Math.random() * 8);
+function randomNine() {
+  return Math.floor(Math.random() * 9);
 }
-function randomTwentyFive() {
-  return Math.floor(Math.random() * 25);
+function randomTwentySix() {
+  return Math.floor(Math.random() * 26);
 }
 
 function generatName() {
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var char1 = alphabet[randomTwentyFive()];
-  var char2 = alphabet[randomTwentyFive()];
-  var number1 = numbers[randomEight()];
-  var number2 = numbers[randomEight()];
-  var number3 = numbers[randomEight()];
+  var char1 = alphabet[randomTwentySix()];
+  var char2 = alphabet[randomTwentySix()];
+  var number1 = numbers[randomNine()];
+  var number2 = numbers[randomNine()];
+  var number3 = numbers[randomNine()];
   var newName = char1+char2+number1+number2+number3;
   return newName;
 }
