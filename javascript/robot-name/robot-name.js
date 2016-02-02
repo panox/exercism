1fc66771f01d5a27012937be724e76ffabf3ae7a
panox <panospalos@gmail.com>
2016-02-02 21:02:30 +0200

    increased the count

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -5,11 +5,11 @@ var Robot = function() {
 function generatName() {
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z' ];
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var char1 = alphabet[Math.floor(Math.random() * 24)];
-  var char2 = alphabet[Math.floor(Math.random() * 24)];
-  var number1 = numbers[Math.floor(Math.random() * 8)];
-  var number2 = numbers[Math.floor(Math.random() * 8)];
-  var number3 = numbers[Math.floor(Math.random() * 8)];
+  var char1 = alphabet[Math.floor(Math.random() * 25)];
+  var char2 = alphabet[Math.floor(Math.random() * 25)];
+  var number1 = numbers[Math.floor(Math.random() * 9)];
+  var number2 = numbers[Math.floor(Math.random() * 9)];
+  var number3 = numbers[Math.floor(Math.random() * 9)];
   return char1+char2+number1+number2+number3;
 }
 
