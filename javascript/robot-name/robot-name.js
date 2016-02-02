025e71399b8c19e3c3bcbd86375642e8e65faa6a
panox <panospalos@gmail.com>
2016-02-02 21:09:00 +0200

    random methods

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -2,15 +2,23 @@ var Robot = function() {
   this.name = generatName();
 };
 
+function randomEight() {
+  return Math.floor(Math.random() * 8);
+}
+function randomTwentyFive() {
+  return Math.floor(Math.random() * 25);
+}
+
 function generatName() {
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var char1 = alphabet[Math.floor(Math.random() * 25)];
-  var char2 = alphabet[Math.floor(Math.random() * 25)];
-  var number1 = numbers[Math.floor(Math.random() * 8)];
-  var number2 = numbers[Math.floor(Math.random() * 8)];
-  var number3 = numbers[Math.floor(Math.random() * 8)];
-  return char1+char2+number1+number2+number3;
+  var char1 = alphabet[randomTwentyFive()];
+  var char2 = alphabet[randomTwentyFive()];
+  var number1 = numbers[randomEight()];
+  var number2 = numbers[randomEight()];
+  var number3 = numbers[randomEight()];
+  var newName = char1+char2+number1+number2+number3;
+  return newName;
 }
 
 Robot.prototype.reset = function () {
