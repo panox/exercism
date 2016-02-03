62400fba232eea17475fe7b7265f2c71b1785c3f
panox <panospalos@gmail.com>
2016-02-03 21:35:24 +0000

    loop through object and array

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -2,6 +2,11 @@ var ETL = function() {};
 
 ETL.prototype.transform = function(old) {
   var obj = {};
+  for (var oldKey in old) {
+    old[oldKey].forEach(function (item) {
+      obj[item] = oldKey;
+    });
+  }
   return obj;
 };
 
