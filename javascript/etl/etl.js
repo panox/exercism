3b95880c838404b5450cab42fee53f734936cfbc
panox <panospalos@gmail.com>
2016-02-07 18:00:26 +0000

    convert to lowercase

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -4,6 +4,7 @@ ETL.prototype.transform = function(old) {
   var obj = {};
   for (var oldKey in old) {
     old[oldKey].forEach(function (item) {
+      item = item.toLowerCase();
       obj[item] = parseInt(oldKey);
     });
   }
