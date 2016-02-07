1070fa0b318964c575297d12a1dc38959d677176
panox <panospalos@gmail.com>
2016-02-07 18:33:56 +0000

    returns float number with many decimals

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -2,4 +2,9 @@ var SpaceAge = function(seconds) {
   this.seconds = seconds;
 };
 
+SpaceAge.prototype.onEarth = function() {
+  var seconds = this.seconds;
+  return seconds/31557600;
+};
+
 module.exports = SpaceAge;
