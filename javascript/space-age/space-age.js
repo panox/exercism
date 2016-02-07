469d3d97235bacee08cc0535cafa4a1085f0a29c
panox <panospalos@gmail.com>
2016-02-07 18:42:18 +0000

    venus pass

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -7,7 +7,12 @@ SpaceAge.prototype.onEarth = function() {
   return parseFloat((seconds/31557600).toFixed(2));
 };
 SpaceAge.prototype.onMercury = function() {
-  
+  var seconds = this.seconds;
+  return parseFloat((seconds/(31557600*0.2408467)).toFixed(2));
+};
+SpaceAge.prototype.onVenus = function() {
+  var seconds = this.seconds;
+  return parseFloat((seconds/(31557600*0.61519726)).toFixed(2));
 };
 
 module.exports = SpaceAge;
