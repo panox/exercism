5181e990987c6ffdcbee23905becea6b0cf7ab3a
panox <panospalos@gmail.com>
2016-02-07 18:48:56 +0000

    object of all the years

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,5 +1,16 @@
 var SpaceAge = function(seconds) {
   this.seconds = seconds;
+  this.earthYearSec = 31557600;
+  this.yearSec = {
+    'Earth'   : this.earthYearSec,
+    'Mercury' : this.earthYearSec * 0.2408467,
+    'Venus'   : this.earthYearSec * 0.61519726,
+    'Mars'    : this.earthYearSec * 1.8808158,
+    'Jupiter' : this.earthYearSec * 11.862615,
+    'Saturn'  : this.earthYearSec * 29.447498,
+    'Uranus'  : this.earthYearSec * 84.016846,
+    'Neptune' : this.earthYearSec * 164.79132
+  };
 };
 
 SpaceAge.prototype.onEarth = function() {
