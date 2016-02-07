b276698ca93630d0729b9e0aedbfde8517f1a774
panox <panospalos@gmail.com>
2016-02-07 18:38:20 +0000

    mercury method

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -6,5 +6,8 @@ SpaceAge.prototype.onEarth = function() {
   var seconds = this.seconds;
   return parseFloat((seconds/31557600).toFixed(2));
 };
+SpaceAge.prototype.onMercury = function() {
+  
+};
 
 module.exports = SpaceAge;
